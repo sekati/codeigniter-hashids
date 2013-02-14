183cd66f4e46ae69bb5e3c24f910be24f03d6d4a
Jason Horwitz <jason@sekati.com>
2013-02-14 18:01:15 -0500

    Case sensitivity of require

--- helpers/hashids_helper.php
+++ helpers/hashids_helper.php
@@ -8,14 +8,14 @@
  * @copyright   Copyright (c) 2013, Sekati LLC.
  * @license     http://www.opensource.org/licenses/mit-license.php
  * @link        http://sekati.com
- * @version     v1.0.2
+ * @version     v1.0.3
  * @filesource
  *
  * @usage       $autoload['config'] = array('hashids');
  *              $autoload['helper'] = array('hashids');
  */
 
-define('HASHIDS_VERSION', '1.0.2');
+define('HASHIDS_VERSION', '1.0.3');
 
 require_once FCPATH . 'sparks/sk-hashids/' . HASHIDS_VERSION . '/vendor/hashids.php';
 
